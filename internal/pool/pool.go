072a2d8d37616cb9eef63a9217b5c88cd1276c8e
zhangji <zhangjigogo@gmail.com>
2019-07-26 14:48:27 +0800

    pool

--- internal/pool/pool.go
+++ internal/pool/pool.go
@@ -26,34 +26,37 @@ var timers = sync.Pool{
 // Stats 包含池状态信息和统计信息
 type Stats struct {
 	// 获取到空闲连接的次数
-	Hits     uint32 // number of times free connection was found in the pool
+	Hits uint32 // number of times free connection was found in the pool
 	// 没有获取到空闲连接的次数
-	Misses   uint32 // number of times free connection was NOT found in the pool
+	Misses uint32 // number of times free connection was NOT found in the pool
 	// 超时次数
 	Timeouts uint32 // number of times a wait timeout occurred
 
 	// 池中总连接数
 	TotalConns uint32 // number of total connections in the pool
 	// 池中空闲连接数
-	IdleConns  uint32 // number of idle connections in the pool
-	// 从池中移除的连接数
+	IdleConns uint32 // number of idle connections in the pool
+	// 从池中移除的陈旧无用连接数
 	StaleConns uint32 // number of stale connections removed from the pool
 }
 
 type Pooler interface {
-	// 建立连接和关闭连接
+	// 基础连接管理: 建立连接和关闭连接
+	//    这里提供了一种仅仅是新建一个连接的方式,通过这种方式建立的连接不会被放到连接
+	//    池中,使用完后要执行 CloseConn 函数关闭连接,即使调用 Put 或 Remove 函数,
+	//    也仅仅相当于执行了  CloseConn 操作
 	NewConn(context.Context) (*Conn, error)
 	CloseConn(*Conn) error
 
-	// 连接池存取删Conn
+	// 连接池连接管理: 从池中对连接进行存取和删除
 	Get(context.Context) (*Conn, error)
 	Put(*Conn)
 	Remove(*Conn)
 
-	// 监控统计
-	Len() int
-	IdleLen() int
-	Stats() *Stats
+	// 监控统计信息
+	Len() int      // 实时连接池大小
+	IdleLen() int  // 实时空闲连接数大小
+	Stats() *Stats // 详细状态信息(下一小节介绍)
 
 	// 关闭连接池
 	Close() error
@@ -64,33 +67,27 @@ type Options struct {
 	Dialer  func(c context.Context) (net.Conn, error)
 	OnClose func(*Conn) error
 
-	PoolSize           int  // 池大小
-	MinIdleConns       int  // 最小空闲数
-	MaxConnAge         time.Duration
-	PoolTimeout        time.Duration
-	IdleTimeout        time.Duration
+	PoolSize           int           // 池大小
+	MinIdleConns       int           // 最小空闲数
+	MaxConnAge         time.Duration // 一个连接的最长生命时长
+	PoolTimeout        time.Duration // 连接池的过期时间
+	IdleTimeout        time.Duration // 一个空闲连接的过期时间
 	IdleCheckFrequency time.Duration
 }
 
 type ConnPool struct {
-	opt *Options // 初始化配置项
-
-	dialErrorsNum uint32 // atomic 连接错误次数
-
-	lastDialErrorMu sync.RWMutex
-	lastDialError   error // 连接错误的最后一次的错误
-
-	queue chan struct{} // 与连接池 size 一样的 channel
-
-	connsMu      sync.Mutex
-	conns        []*Conn // 建立过的所有连接 conns
-	idleConns    []*Conn // 空闲的(在连接池中没有被使用的) conns
-	poolSize     int  // 实时池大小
-	idleConnsLen int
-
-	stats Stats
-
-	_closed uint32 // atomic  连接池是否被关闭
+	opt             *Options      // 配置项
+	dialErrorsNum   uint32        // 建立连接错误次数(原子操作)
+	lastDialErrorMu sync.RWMutex  // 并发操作 lastDialError 的读写锁
+	lastDialError   error         // 最后一次建立连接发生的错误
+	queue           chan struct{} // 与连接池 size 一样的 channel, 当没有空闲连接时,用于阻塞获取连接的请求
+	connsMu         sync.Mutex    // 并发操作 conns,idleConns 的互斥锁
+	conns           []*Conn       // 连接池 conns
+	idleConns       []*Conn       // 空闲的(在连接池中没有被使用的) conns
+	poolSize        int           // 实时连接池大小
+	idleConnsLen    int           // 实时空闲连接数
+	stats           Stats         // 连接池状态信息
+	_closed         uint32        // 连接池是否被关闭(原子操作)
 }
 
 // ConnPool 实现 Pooler 接口
@@ -105,12 +102,13 @@ func NewConnPool(opt *Options) *ConnPool {
 		idleConns: make([]*Conn, 0, opt.PoolSize),
 	}
 
-	// 如果配置了最小空闲数,则建立最小数的空闲连接
+	// 检查最小空闲连接: 如果配置了最小空闲数,则建立最小数的空闲连接
 	for i := 0; i < opt.MinIdleConns; i++ {
+		// 空闲连接数小于配置时,每次执行check只会新建一个连接
 		p.checkMinIdleConns()
 	}
 
-	// 定时任务，清理过期的conn
+	// 清理过期 conn
 	if opt.IdleTimeout > 0 && opt.IdleCheckFrequency > 0 {
 		go p.reaper(opt.IdleCheckFrequency)
 	}
@@ -123,6 +121,7 @@ func (p *ConnPool) checkMinIdleConns() {
 	if p.opt.MinIdleConns == 0 {
 		return
 	}
+	// 当前池的总连接数在 opt.PoolSize 内的前提下,需要考虑最小空闲连接数的情况
 	if p.poolSize < p.opt.PoolSize && p.idleConnsLen < p.opt.MinIdleConns {
 		p.poolSize++
 		p.idleConnsLen++
@@ -143,7 +142,7 @@ func (p *ConnPool) addIdleConn() {
 	p.connsMu.Unlock()
 }
 
-// NewConn 实现Pooler接口的 NewConn 方法
+// NewConn 实现Pooler接口的 NewConn 方法, 新建一个连接
 func (p *ConnPool) NewConn(ctx context.Context) (*Conn, error) {
 	return p._NewConn(ctx, false)
 }
@@ -182,19 +181,19 @@ func (p *ConnPool) newConn(ctx context.Context, pooled bool) (*Conn, error) {
 	netConn, err := p.opt.Dialer(ctx)
 	if err != nil {
 		p.setLastDialError(err)
-		// 当错误数超过 poolSize 后,开始重试连接
+		// 当错误数等于 poolSize 后,开始重试连接
 		if atomic.AddUint32(&p.dialErrorsNum, 1) == uint32(p.opt.PoolSize) {
 			go p.tryDial()
 		}
 		return nil, err
 	}
 
-	cn := NewConn(netConn)
+	cn := NewConn(netConn) // todo
 	cn.pooled = pooled
 	return cn, nil
 }
 
-// 恢复连接
+// 重试连接,如果连接出错,则每秒钟尝试新建一个连接,直到连接建立成功
 func (p *ConnPool) tryDial() {
 	for {
 		if p.closed() {
@@ -234,12 +233,13 @@ func (p *ConnPool) Get(ctx context.Context) (*Conn, error) {
 		return nil, ErrClosed
 	}
 
-	//等待有空闲,通过channel方式同步，如果连接池满了，则会阻塞等待，但是不会一直阻塞，有一个超时机制
+	// 等待有空闲,通过channel方式同步，如果连接池满了，则会阻塞等待，但是不会一直阻塞，有一个超时机制, ctx 中要包含超时条件
 	err := p.waitTurn(ctx)
 	if err != nil {
 		return nil, err
 	}
 
+	// 从空闲 conns 中获取 conn
 	for {
 		p.connsMu.Lock()
 		cn := p.popIdle()
@@ -249,9 +249,10 @@ func (p *ConnPool) Get(ctx context.Context) (*Conn, error) {
 			break
 		}
 
-		// 取出空闲的，并判断conn是否过期
+		// 判断conn是否过期
 		if p.isStaleConn(cn) {
 			_ = p.CloseConn(cn)
+			// 关闭超时 conn 后继续获取
 			continue
 		}
 
@@ -259,7 +260,9 @@ func (p *ConnPool) Get(ctx context.Context) (*Conn, error) {
 		return cn, nil
 	}
 
-	// 连接池中没有空闲连接了, misses 数加 1
+	// 连接池中没有空闲连接了, 需要新建连接
+
+	// misses 数加 1
 	atomic.AddUint32(&p.stats.Misses, 1)
 
 	//创建新的连接
@@ -276,7 +279,7 @@ func (p *ConnPool) getTurn() {
 	p.queue <- struct{}{}
 }
 
-// p.queue是Get方法成功的时候就往channel里面写，Put方法成功就往channel里面读，释放出来位置
+// p.queue 是 Get方法成功的时候就往channel里面写，Put方法成功就往channel里面读，释放出来位置
 func (p *ConnPool) waitTurn(ctx context.Context) error {
 	var done <-chan struct{}
 	if ctx != nil {
@@ -285,13 +288,17 @@ func (p *ConnPool) waitTurn(ctx context.Context) error {
 
 	select {
 	case <-done:
+		// 在调用 Get 方法时在 ctx 中设置的超时机制
 		return ctx.Err()
 	case p.queue <- struct{}{}:
 		return nil
 	default:
+		// 默认的超时机制
 		timer := timers.Get().(*time.Timer)
+		// 从临时对象池中获取一个 timer, 用于设置连接池的超时时间
 		timer.Reset(p.opt.PoolTimeout)
 
+		// 双重检查
 		select {
 		case <-done:
 			// 临界点处理:防止此时正好有ticker写入了timer.C,但是select到了这一个case里面
@@ -324,11 +331,12 @@ func (p *ConnPool) popIdle() *Conn {
 	if len(p.idleConns) == 0 {
 		return nil
 	}
-
+	// 去最后一个空闲连接
 	idx := len(p.idleConns) - 1
 	cn := p.idleConns[idx]
 	p.idleConns = p.idleConns[:idx]
 	p.idleConnsLen--
+	// 每获取一个连接,就检查一次最少空闲连接数
 	p.checkMinIdleConns()
 	return cn
 }
@@ -344,7 +352,7 @@ func (p *ConnPool) Put(cn *Conn) {
 	p.idleConns = append(p.idleConns, cn)
 	p.idleConnsLen++
 	p.connsMu.Unlock()
-	// Get成功时候是写入channel，这个时候自然是释放channel
+	// Get成功时候是写入channel，Put 成功后释放channel
 	p.freeTurn()
 }
 
@@ -414,6 +422,7 @@ func (p *ConnPool) Stats() *Stats {
 	}
 }
 
+// 判断连接池是否已被关闭
 func (p *ConnPool) closed() bool {
 	return atomic.LoadUint32(&p._closed) == 1
 }
@@ -462,7 +471,7 @@ func (p *ConnPool) reaper(frequency time.Duration) {
 		if p.closed() {
 			break
 		}
-		// 回收超时连接
+		// 从空闲连接中，回收超时连接
 		n, err := p.ReapStaleConns()
 		if err != nil {
 			internal.Logger.Printf("ReapStaleConns failed: %s", err)
@@ -476,15 +485,16 @@ func (p *ConnPool) reaper(frequency time.Duration) {
 func (p *ConnPool) ReapStaleConns() (int, error) {
 	var n int
 	for {
-		// 往 p.queue channel 里面写入一个 struct{}{} ,表示占用一个任务
+		// 往 p.queue channel 里面写入一个 struct{}{} ,表示占用一个任务,准备读取一个空闲连接
 		p.getTurn()
 
 		p.connsMu.Lock()
 		cn := p.reapStaleConn()
 		p.connsMu.Unlock()
-		// 处理完了，释放占用的channel的位置
+		// 处理完了，释放占用的channel的位置,表示读取空闲连接结束
 		p.freeTurn()
 
+		// todo 这里会不会发生遇到一个未超时连接,就返回nil, 停止检查后面的连接, idleConns 是不是保证超时的连接都在 slice 的前面
 		if cn != nil {
 			p.closeConn(cn)
 			n++
@@ -495,6 +505,7 @@ func (p *ConnPool) ReapStaleConns() (int, error) {
 	return n, nil
 }
 
+// 清理无用连接
 func (p *ConnPool) reapStaleConn() *Conn {
 	if len(p.idleConns) == 0 {
 		return nil
